ce1de219535e89f7d784ec86790ebdad33d49bbe
Adam <aburabu@mail.ru>
2025-12-16 20:53:34 +0300

    Timeline. Реализовано разбиение основного компонента с выдающим списком на несколько отдельных

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { XMarkIcon } from '@heroicons/vue/24/outline';
+</script>
+
+<template>
+  <button
+    class="rounded bg-gray-100 p-3 enable:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
+  >
+    <XMarkIcon class="h-8" />
+  </button>
+</template>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,27 @@
+<script setup>
+import BaseButton from './BaseButton.vue';
+import { XMarkIcon } from '@heroicons/vue/24/outline';
+
+const { options, placeholder } = defineProps({
+  placeholder: String,
+  selected: Number,
+  options: Object,
+});
+</script>
+
+<template>
+  <BaseButton>
+    <component :is="XMarkIcon" />
+  </BaseButton>
+  <select class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
+    <option selected disabled value="">{{ placeholder }}</option>
+    <option
+      v-for="{ value, label } in options"
+      :key="value"
+      :value="value"
+      :selected="value === selected"
+    >
+      {{ label }}
+    </option>
+  </select>
+</template>
