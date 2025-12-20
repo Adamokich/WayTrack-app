d7b69d39c6f5bb5fb47f429771cc9f4e6c32040b
Adam <aburabu@mail.ru>
2025-12-20 11:52:13 +0300

    Activities.

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,3 +1,44 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+import BaseSelect from '@/components/BaseSelect.vue';
+import { TrashIcon } from '@heroicons/vue/24/outline';
+
+const activities = ['Coding', 'Reading', 'Training'];
+
+const periodSelecOptions = [
+  {
+    value: 15,
+    label: '0:15',
+  },
+  {
+    value: 20,
+    label: '0:20',
+  },
+  {
+    value: 25,
+    label: '0:25',
+  },
+  {
+    value: 30,
+    label: '0:30',
+  },
+];
+</script>
+
 <template>
-  <h2>activities</h2>
+  <div>
+    <ul class="divide-y">
+      <li v-for="activity in activities" :key="activity" class="flex flex-col gap-2 p-4 font-mono">
+        <div class="flex items-center gap-2">
+          <BaseButton>
+            <TrashIcon class="h-8" />
+          </BaseButton>
+          <span class="truncate text-xl">{{ activity }}</span>
+        </div>
+        <div>
+          <BaseSelect placeholder="h:mmm" :options="periodSelecOptions" />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
